d1b9147f135e5df665746245d44ece044765a586
chenchuan <1350581922@qq.com>
2021-01-10 01:02:48 +0800

    打包问题修复

--- src/pages/base/Login.vue
+++ src/pages/base/Login.vue
@@ -50,7 +50,7 @@
                 type="primary"
                 style="width:100%; margin-bottom:30px;"
                 @click.native.prevent="handleLogin"
-            >Sign in</el-button>
+            >登录</el-button>
         </el-form>
     </div>
 </template>
